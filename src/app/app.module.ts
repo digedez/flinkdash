2db15951cb4ef64a54d29a2a35420df8bcea0d7d
Diego <digedez@gmail.com>
2017-10-20 13:53:46 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,49 @@ import { AppComponent } from './app.component';
 import { MenuflinkComponent } from './components/menuflink/menuflink.component';
 import { SidebarflinkComponent } from './components/sidebarflink/sidebarflink.component';
 
+import { ContentComponent } from './components/content/content.component';
+import { ContentHomeComponent } from './components/content/content-home/content-home.component';
+import { ContentTarjetaComponent } from './components/content/content-tarjeta/content-tarjeta.component';
+import { ContentAdmComponent } from './components/content/content-adm/content-adm.component';
+import { ContentMetasComponent } from './components/content/content-metas/content-metas.component';
+import { ContentPrestamoComponent } from './components/content/content-prestamo/content-prestamo.component';
+import { ContentTransfComponent } from './components/content/content-transf/content-transf.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+//RUTAS
+import { APP_ROUTING } from './app.routes';
+
+//SERVICIOS
+//import { HomeService } from './services/home.service';
+
+//GRAFICOS
+//import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+
+//MAPA
+//import { AgmCoreModule } from '@agm/core';
+
+//Auth Guard
+//import { AuthGuard } from './_guards/index';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuflinkComponent,
     SidebarflinkComponent,
+    ContentHomeComponent,
+    ContentTarjetaComponent,
+    ContentAdmComponent,
+    ContentMetasComponent,
+    ContentPrestamoComponent,
+    ContentTransfComponent,
+    LoginComponent,
+    ContentComponent,
+    LineaComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
